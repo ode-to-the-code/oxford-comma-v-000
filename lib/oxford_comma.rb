37b47e0144ace276d8dd4909e2a59d5f1d1c054d
Jad Abdallah <justcallmethis@gmail.com>
2018-11-25 16:09:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
 def oxford_comma(array)
-
+  last = array.pop
+  array.join(", ") << ", and " << last
+  # could use if then statement to model the rules of oxford comma in the tests.
+  # like don't do it if there are only two items.
 end
